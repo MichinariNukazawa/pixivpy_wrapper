fbdd9398d072422d247cf4fc00e70d51b906c3d8
MichinariNukazawa <michinari.nukazawa@gmail.com>
2018-04-24 21:25:04 +0900

    latest log rotation when start

--- azure.sh
+++ azure.sh
@@ -8,7 +8,7 @@ trap 'echo "error:$0($LINENO) \"$BASH_COMMAND\" \"$@\""' ERR
 TAG="アズールレーン"
 DATE=$(date +'%Y%m%d%H%M%S')
 
-echo "start: $(date +'%Y-%m-%d %H:%M:%S')" >> download_latest.log 2>&1
+echo "start: $(date +'%Y-%m-%d %H:%M:%S')" > download_latest.log 2>&1
 
 python3 pixiv_tag_download.py "${TAG}" "${HOME}/pixiv_data/image_" >> download_latest.log 2>&1
 RES=$?
